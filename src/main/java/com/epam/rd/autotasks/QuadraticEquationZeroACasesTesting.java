c4cd7604f7a13da5e3a2d50c08861257ec7e9388
Anych <anuar123@mail.ru>
2022-08-14 16:52:58 +0600

    Finish to do QuadraticEquationZeroACasesTesting and all class have finished

--- src/main/java/com/epam/rd/autotasks/QuadraticEquationZeroACasesTesting.java
+++ src/main/java/com/epam/rd/autotasks/QuadraticEquationZeroACasesTesting.java
@@ -1,15 +1,37 @@
 package com.epam.rd.autotasks;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(Parameterized.class)
 public class QuadraticEquationZeroACasesTesting {
     protected QuadraticEquation quadraticEquation = new QuadraticEquation();
-
+    private double a;
+    private double b;
+    private double c;
     public QuadraticEquationZeroACasesTesting(double a, double b, double c) {
-
+        this.a = a;
+        this.b = b;
+        this.c = c;
     }
 
-
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {0, 1, 2},
+                {0, 3, 4},
+                {0, 5, 6},
+                {0, 7, 8}
+        });
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testNoRootsCase() {
+        assertEquals(new IllegalArgumentException(), quadraticEquation.solve(a, b, c));
+    }
 }
