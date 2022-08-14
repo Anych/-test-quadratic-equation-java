054733d0b6b6182faa9c02a9ba6fa2a4c9e00f22
Anych <anuar123@mail.ru>
2022-08-14 16:13:01 +0600

    Finish to do Quadratic Equation class

--- src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
+++ src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
@@ -1,8 +1,26 @@
 package com.epam.rd.autotasks;
 
 public class QuadraticEquation {
-    public String solve(double a, double b, double c) {
-        throw new UnsupportedOperationException();
+    public String solve(double a, double b, double c) throws IllegalArgumentException {
+        if (a == 0) {
+            throw new IllegalArgumentException();
+        }
+        double discriminant = Math.pow(b, 2) - 4 * a * c;
+
+        if (discriminant > 0)
+        {
+            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            return root1 + " " + root2;
+        }
+        else if (discriminant == 0)
+        {
+            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+
+            return Double.toString(root1);
+        }
+        else
+            return "no roots";
     }
 
 }
